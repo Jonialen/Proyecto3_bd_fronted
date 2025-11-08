d694c64972753c85edc03bf5ecc359ddcd7d298a
Jonialen <jonathanalej09@gmail.com>
2025-11-07 23:36:39 -0600

    new file:   .env.example modified:   .gitignore modified:   src/services/Axios.js

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create(
     {
-        baseURL:'https://bd-api.eduvial.space/api',
+        baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
         withCredentials: false
     }
 )
